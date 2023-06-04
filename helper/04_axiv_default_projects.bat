b6a308ba77d98cbe324f83ccf02dc79e9849329e
Waldemar Handy <waldemar.handy@bshg.com>
2023-06-04 22:02:17 +0200

    helper batch-es added

---
+++ helper/04_axiv_default_projects.bat
@@ -0,0 +1,18 @@
+
+@echo =============== Start ==============================
+
+:: axiv default projects
+@echo ----------------------------------------------------
+@echo ---------------- Axiv ------------------------------
+@echo ----------------------------------------------------
+
+::@call make axstyle project=variant_efr32bg21 -j6 -B
+::@call make axstyle project=variant_mkv56f1 -j6 -B
+::@call make axstyle project=variant_ra4m2 -j6 -B
+::@call make axstyle project=variant_stm32g071 -j6 -B
+::@call make axstyle project=variant_stm32g474 -j6 -B
+::@call make axstyle project=variant_stm32h723 -j6 -B
+::@call make axstyle project=variant_stm32l471 -j6 -B
+
+@exit /b
+
