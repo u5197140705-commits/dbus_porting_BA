7922cc08509aa87a9982089ce43271c131ac999f
Waldemar Handy <waldemar.handy@bshg.com>
2023-06-04 22:21:35 +0200

    Update helper content

--- helper/01_compile_default_projects.bat
+++ helper/01_compile_default_projects.bat
@@ -6,13 +6,18 @@
 @echo ---------------- COMPILE ---------------------------
 @echo ----------------------------------------------------
 
-::@call make all project=variant_efr32bg21 -j
-::@call make all project=variant_mkv56f1 -j
-::@call make all project=variant_ra4m2 -j
-::@call make all project=variant_stm32g071 -j
-::@call make all project=variant_stm32g474 -j
-::@call make all project=variant_stm32h723 -j
-::@call make all project=variant_stm32l471 -j
+@call ..\make all project=variant_efr32bg21 -j
+@call ..\make all project=variant_mkv56f1 -j
+@call ..\make all project=variant_ra4m2 -j
+@call ..\make all project=variant_stm32g071 -j
+@call ..\make all project=variant_stm32g474 -j
+@call ..\make all project=variant_stm32h723 -j
+@call ..\make all project=variant_stm32l471 -j
 
 @exit /b
+
+
+:: help content:
+:: To crate a log file use this command
+:: 01_compile_default_projects.bat >compile_log.txt 2>&1
 
